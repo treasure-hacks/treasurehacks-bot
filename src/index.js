b48884f838be446f68da8d2889b7e610d05883cf
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2022-04-07 13:52:52 -0700

    add base for invite tracking

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ const { Routes } = require('discord-api-types/v9')
 const fs = require('fs')
 const { Client, Intents, Collection } = require('discord.js')
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS
+  ]
 }) // Connect to our discord bot
 const commands = new Collection() // Where the bot (slash) commands will be stored
-const commandarray = [] // Array to store commands for sending to the REST API
+const commandArray = [] // Array to store commands for sending to the REST API
 const token = process.env.DISCORD_TOKEN
 
 function registerSlashCommands () {
@@ -20,7 +24,7 @@ function registerSlashCommands () {
   for (const file of commandFiles) {
     const command = require(`./commands/${file}`) // Get and define the command file.
     commands.set(command.data.name, command) // Set the command name and file for handler to use.
-    commandarray.push(command.data.toJSON()) // Push the command data to an array (for sending to the API).
+    commandArray.push(command.data.toJSON()) // Push the command data to an array (for sending to the API).
   }
 
   const rest = new REST({ version: '9' }).setToken(token);
@@ -29,7 +33,7 @@ function registerSlashCommands () {
     try {
       console.log('Refreshing application (/) commands...')
       await rest.put(Routes.applicationCommands(client.user.id), {
-        body: commandarray
+        body: commandArray
       })
       console.log('Successfully reloaded application (/) commands.')
     } catch (error) {
@@ -53,11 +57,56 @@ async function respondToCommand (interaction) {
   }
 }
 
-client.once('ready', registerSlashCommands)
+// Track Invites
+const invites = new Collection()
+
+async function loadInvites () {
+  // Cache invites
+  client.guilds.cache.forEach(async guild => {
+    // Fetch all Guild Invites
+    const firstInvites = await guild.invites.fetch()
+    invites.set(guild.id, new Collection(firstInvites.map((invite) => [invite.code, invite.uses])))
+  })
+}
+function trackInvites (client) {
+  client.on('inviteDelete', (invite) => {
+    // Delete the Invite from Cache
+    invites.get(invite.guild.id).delete(invite.code)
+  })
+  client.on('inviteCreate', (invite) => {
+    // Update cache on new invites
+    invites.get(invite.guild.id).set(invite.code, invite.uses)
+  })
+  client.on('guildCreate', (guild) => {
+    // We've been added to a new Guild. Let's fetch all the invites, and save it to our cache
+    guild.invites.fetch().then(guildInvites => {
+      // This is the same as the ready event
+      invites.set(guild.id, new Map(guildInvites.map((invite) => [invite.code, invite.uses])))
+    })
+  })
+  client.on('guildDelete', (guild) => {
+    // We've been removed from a Guild. Let's delete all their invites
+    invites.delete(guild.id)
+  })
+}
+trackInvites(client)
+
+client.once('ready', () => {
+  registerSlashCommands()
+  loadInvites()
+})
 client.on('interactionCreate', async interaction => {
   // Command handler
   if (!interaction.isCommand()) return
   respondToCommand(interaction)
+})
+client.on('guildMemberAdd', async member => {
+  const newInvites = await member.guild.invites.fetch()
+  // This is the *existing* invites for the guild.
+  const oldInvites = invites.get(member.guild.id)
+  // Look through the invites, find the one for which the uses went up.
+  const invite = newInvites.find(i => i.uses > oldInvites.get(i.code))
+  console.log(invite)
 })
 
 client.login(token)
