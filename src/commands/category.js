626e31182f247325049f1c929e5aa8f7d755e7e2
Awesome_E <54484616+Awesome-E@users.noreply.github.com>
2022-05-07 17:16:47 -0700

    add bulk category commands

---
+++ src/commands/category.js
@@ -0,0 +1,178 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { ChannelType } = require('discord-api-types/v9')
+const { Permissions } = require('discord.js')
+
+function clearCategory (interaction, client) {
+  const guild = interaction.guild
+  const category = interaction.options.getChannel('category')
+  const deleteParent = interaction.options.getBoolean('delete')
+  const channelList = guild.channels.cache.filter(c => c.parentId === category.id)
+  const embed = {
+    title: 'Clearing Category',
+    description: `Deleting the following categories:\n${channelList.map(c => `<#${c.id}>`).join('\n')}`
+  }
+  interaction.reply({
+    embeds: [embed],
+    ephemeral: true
+  })
+  channelList.forEach(channel => {
+    channel.delete()
+  })
+  if (deleteParent) category.delete()
+}
+
+function syncCategory (interaction, client) {
+  const guild = interaction.guild
+  const category = interaction.options.getChannel('category')
+  const deleteParent = interaction.options.getBoolean('delete')
+  const channelList = guild.channels.cache.filter(c => c.parentId === category.id)
+  const embed = {
+    title: 'Synced Category',
+    description: `The following channels should now have the same permissions as \`${category.name}\`:\n` +
+      channelList.map(c => `<#${c.id}>`).join(', '),
+    color: 0x00ffaa
+  }
+  interaction.reply({
+    embeds: [embed],
+    ephemeral: true
+  })
+  channelList.forEach(channel => {
+    channel.lockPermissions()
+  })
+  if (deleteParent) category.delete()
+}
+
+async function teamCategory (interaction, client) {
+  const category = interaction.options.getChannel('category')
+  const role = interaction.options.getRole('role')
+
+  // Refresh Member Roles
+  await interaction.guild.members.fetch()
+  const members = role?.members?.map(m => m)?.sort((a, b) => Math.random() < 0.5 ? -1 : 1)
+  const size = interaction.options.getInteger('size')
+
+  const channelType = interaction.options.getString('channel-type') || 'text'
+
+  if (!members?.length) {
+    interaction.reply({
+      embeds: [{
+        title: 'Error',
+        description: 'No users in role to divide',
+        color: 0xff0000
+      }],
+      ephemeral: true
+    })
+    return
+  }
+
+  const teams = members.reduce((array, item, index) => {
+    if (index % size === 0) array.push([item])
+    else array[array.length - 1].push(item)
+    return array
+  }, [])
+
+  // return interaction.reply(JSON.stringify(teams.map(x => x.map(m => { return { id: m.id, u: m.displayName } }))))
+
+  async function createChannel (name, type, team) {
+    return await interaction.guild.channels.create(name, {
+      type,
+      parent: category.id,
+      permissionOverwrites: [
+        {
+          id: interaction.guild.id, // @everyone
+          deny: [Permissions.FLAGS.VIEW_CHANNEL]
+        },
+        ...team.map(user => {
+          return {
+            id: user.id,
+            allow: [Permissions.FLAGS.VIEW_CHANNEL]
+          }
+        })
+      ]
+    })
+  }
+  teams.forEach(async (team, index) => {
+    index += 1
+    if (channelType !== 'voice') await createChannel('team-' + index, 'GUILD_TEXT', team)
+    if (channelType !== 'text') await createChannel('team-' + index, 'GUILD_VOICE', team)
+  })
+  const readableType = ({
+    text: 'Text',
+    voice: 'Voice',
+    both: 'Text and Voice'
+  })[channelType]
+
+  const embed = {
+    title: `${readableType} Channels Created Successfully`,
+    description: `Successfully created teams for <@&${role.id}>\n\n` +
+      teams.map((team, index) => `Team ${index + 1}: ` + team.map(user => `<@!${user.id}>`).join(', ')).join('\n'),
+    color: 0x00aaff
+  }
+  interaction.reply({
+    embeds: [embed]
+  })
+}
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('category')
+    .setDescription('Manage categories faster than ever before')
+    .addSubcommand(subcommand => {
+      subcommand.setName('clear').setDescription('Removes all channels from a certain category')
+        .addChannelOption(option => option
+          .setName('category')
+          .setDescription('The category to remove all channels from')
+          .addChannelTypes(ChannelType.GuildCategory)
+          .setRequired(true)
+        )
+        .addBooleanOption(option => option
+          .setName('delete')
+          .setDescription('Whether you would like to delete the category after clearing all channels')
+        )
+      return subcommand
+    })
+    .addSubcommand(subcommand => {
+      subcommand.setName('teams').setDescription('Creates teams of a set size from all members in a role and creates a category for each of them')
+        .addChannelOption(option => option
+          .setName('category')
+          .setDescription('The category where these channels should be created')
+          .addChannelTypes(ChannelType.GuildCategory)
+          .setRequired(true)
+        )
+        .addRoleOption(option => option
+          .setName('role')
+          .setDescription('The role you would like to split into teams')
+          .setRequired(true)
+        )
+        .addIntegerOption(option => option
+          .setName('size')
+          .setDescription('The number of people per team')
+          .setRequired(true)
+        )
+        .addStringOption(option => option
+          .setName('channel-type')
+          .setDescription('The types of channels to create (defaults to text)')
+          .addChoices({ name: 'text', value: 'text' }, { name: 'voice', value: 'voice' }, { name: 'both', value: 'both' })
+        )
+      return subcommand
+    })
+    .addSubcommand(subcommand => {
+      subcommand.setName('sync').setDescription('Sync permissions for all channels with their respective category')
+        .addChannelOption(option => option
+          .setName('category')
+          .setDescription('The category whose channels you would like to sync')
+          .addChannelTypes(ChannelType.GuildCategory)
+          .setRequired(true)
+        )
+      return subcommand
+    }),
+  userPermissions: ['ADMINISTRATOR'],
+  defaultMemberPermissions: 8,
+  execute: async (interaction, client) => {
+    switch (interaction.options.getSubcommand()) {
+      case 'clear': return clearCategory(interaction, client)
+      case 'sync': return syncCategory(interaction, client)
+      case 'teams': return teamCategory(interaction, client)
+    }
+  }
+}
