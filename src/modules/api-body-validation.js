0790552dcb327ea621bedc92b4fd3629242b100c
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2022-10-31 01:24:15 -0700

    allow unknown parameters for route

--- src/modules/api-body-validation.js
+++ src/modules/api-body-validation.js
@@ -1,7 +1,7 @@
 const Joi = require('joi')
 
-function validateBody (req, res, schema) {
-  const { error } = Joi.object(schema).validate(req.body)
+function validateBody (req, res, schema, options = {}) {
+  const { error } = Joi.object(schema).validate(req.body, options)
   if (!error) return true
   const errors = error.details.map(d => d.message)
   return res.status(400).send({ errors })
--- src/routes/api/hacker-signup/index.js
+++ src/routes/api/hacker-signup/index.js
@@ -11,7 +11,7 @@ router.post('/google-form', validateAPIKey, async (req, res) => {
     guild: Joi.string().required(),
     discord_tag: Joi.string().regex(/^.*#\d{4}$/).required(),
     role: Joi.string().required()
-  })
+  }, { allowUnknown: true })
   if (!isValid) return
 
   const roleAddResponse = await addToRole(req.body.guild, req.body.discord_tag, req.body.role)
