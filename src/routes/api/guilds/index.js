bd41aa35758cd6cb86c32d99301eb8b96d2002d4
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2023-04-21 12:26:41 -0700

    guild users path

--- src/routes/api/guilds/index.js
+++ src/routes/api/guilds/index.js
@@ -24,4 +24,26 @@ router.get('/:guild/user-ids', validateAPIKey, async (req, res) => {
   res.send(users)
 })
 
+router.get('/:guild/users', validateAPIKey, async (req, res) => {
+  if (req.query.ids == null || !/(\d+(?:,|$))+/.test(req.query.ids)) {
+    return res.status(400).send({ error: 'Users not specified correctly' })
+  }
+  const userIDs = req.query.ids.split(',')
+
+  const guilds = await client.guilds.fetch()
+  const guild = await guilds.get(req.params.guild)?.fetch()
+  if (!guild) return res.status(404).send({ error: 'Bot is not in this guild' })
+
+  // Refresh members
+  const members = await guild.members.fetch()
+  await guild.roles.fetch()
+  const users = members.map(m => {
+    const { id, username, discriminator, tag } = m.user
+    const roles = m.roles.cache.map(r => r.name)
+    return { id, username, discriminator, tag, avatarURL: m.user.avatarURL(), roles }
+  }).filter(u => userIDs.includes(u.id))
+
+  res.send(users)
+})
+
 module.exports = router
