f05cb75f0ad2d0e1f736757e40e47765335f4259
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2023-05-12 09:22:34 -0700

    Nicer format for response

--- src/commands/chat-input/attendance.js
+++ src/commands/chat-input/attendance.js
@@ -19,10 +19,11 @@ async function recordAttendance (interaction, client) {
   serverConfig.attendance = serverConfig.attendance || {}
   await interaction.guild.fetch() // Fetching this refreshes voice states cache
   const voiceStates = interaction.guild.voiceStates.cache.filter(vs => vs.channelId === interaction.channel.id)
-  const members = voiceStates.map(vs => vs.member.user.id)
+  const members = voiceStates.map(vs => vs.member)
   if (members.length === 0) return interaction.reply({ content: 'Error: No members in voice channel', ephemeral: true })
-  serverConfig.attendance[name] = members
-  interaction.reply({ content: serverConfig.attendance[name].join(', '), ephemeral: true })
+  serverConfig.attendance[name] = members.map(m => m.user.id)
+  const message = `Recorded attendance for ${name}: ${members.join(', ')}\n\n\`${members.join(', ')}\``
+  interaction.reply({ content: message, ephemeral: true })
   await serverSettingsDB.put(serverConfig)
 }
 
