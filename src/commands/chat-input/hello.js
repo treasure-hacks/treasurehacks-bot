44cb505449884890e6dcfd758a2d69bb7c164df0
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2023-05-12 10:24:42 -0700

    Improvements to /hello
    - Use fetch instead of axios
    - lowercase the first word of the fact if it is a common starting word
    - Make the first sentence a question instead of the whole thing a question (some facts have >1 sentence)

--- src/commands/chat-input/hello.js
+++ src/commands/chat-input/hello.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line no-unused-vars
 const { Client, ChatInputCommandInteraction, SlashCommandBuilder } = require('discord.js')
-const axios = require('axios')
 
 /**
  * Says hello to the user, and provides a random fact
@@ -9,13 +8,16 @@ const axios = require('axios')
  */
 async function sayHello (interaction, client) {
   const greetings = ['Hey there!', 'Hello!', 'What\'s up?', 'Welcome to Treasure Hacks!', 'How\'s it going?', 'Hi!']
-  const fact = await axios({
-    method: 'GET',
-    url: 'https://uselessfacts.jsph.pl/random.json?language=en'
-  }).catch(e => {})
-  if (!fact?.data) return interaction.reply(greetings[Math.floor(Math.random() * greetings.length)])
+  const fact = await fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+    .then(x => x.json()).catch(() => {})
+  if (!fact) return interaction.reply(greetings[Math.floor(Math.random() * greetings.length)])
+  const factText = fact.text
+    .replace(/([^.]{15})[.!?](?=\s+[A-Z]|s*$)/, '$1?') // Replace first sentence with a question
+    .replace(/`/g, '\'') // Replace backticks with single quotes
+    .replace(/^(the|a|since|until|in|out|when|if|over|under|about|there|here|it|you|this|most|some|all)\b/i,
+      m0 => m0.toLowerCase()) // Lowercase the most common first words of the fact
   return interaction.reply(`${greetings[Math.floor(Math.random() * greetings.length)]} ` +
-    `Did you know that ${fact.data.text.replace(/[.!?]\s*$/, '').replace(/`/g, '\'')}?`)
+    `Did you know that ${factText}`)
 }
 
 module.exports = {
