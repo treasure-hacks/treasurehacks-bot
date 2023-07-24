88485412b4f2ca329691227970f96c9594b3e6b8
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2023-07-23 17:16:44 -0700

    Allow posting threads without message

--- src/routes/api/threads/index.js
+++ src/routes/api/threads/index.js
@@ -12,27 +12,38 @@ router.post('/create', validateAPIKey, async (req, res) => {
   const isValid = validateBody(req, res, {
     guild: Joi.string().required(),
     channel: Joi.string().regex(/^\d+$/).required(),
-    message: Joi.string().required(),
-    name: Joi.string().required()
+    message: Joi.string(),
+    name: Joi.string().required(),
+    type: Joi.when('message', {
+      is: Joi.string().required(),
+      then: Joi.forbidden(),
+      otherwise: Joi.string().valid('PublicThread', 'PrivateThread')
+    })
   })
   if (!isValid) return
 
-  const { guild, channel, message: messageID, name } = req.body
+  const { guild, channel: channelID, message: messageID, name } = req.body
   /** @type {Message | null} */
-  const message = await client.guilds.fetch(guild)
-    .then(async g => await g.channels.fetch(channel))
-    .then(async c => await c.messages.fetch(messageID))
+  const channel = await client.guilds.fetch(guild)
+    .then(async g => await g.channels.fetch(channelID))
     .catch(() => null)
-  if (!message) return res.status(404).send({ error: 'Message not found' })
+  if (!channel) return res.status(404).send({ error: 'Channel not found' })
+  const message = messageID && await channel.messages.fetch(messageID).catch(() => null)
+  if (messageID && !message) return res.status(404).send({ error: 'Message not found' })
 
   /** @type {GuildTextThreadManager | undefined} */
-  const threads = message.channel.threads
+  const threads = channel.threads
   if (!threads) return res.status(400).send({ error: 'Cannot create threads in this channel type' })
 
-  const existingThread = await threads.fetch(message.id).catch(() => null)
-  if (existingThread) res.status(409).send({ error: 'Message already has a thread' })
-
-  const thread = await threads.create({ startMessage: message, name }).catch(() => null)
+  let thread
+  if (message) {
+    const existingThread = await threads.fetch(message.id).catch(() => null)
+    if (existingThread) return res.status(409).send({ error: 'Message already has a thread' })
+    thread = await threads.create({ startMessage: message, name }).catch(() => null)
+  } else {
+    const type = ChannelType[req.body.type || 'PublicThread']
+    thread = await threads.create({ type, name }).catch(() => null)
+  }
   res.send(thread?.toJSON())
 })
 
